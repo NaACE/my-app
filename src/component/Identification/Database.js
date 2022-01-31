fe1226433a133f2f43baa53cf33247551f5c0e01
NaACE <36949980+NaACE@users.noreply.github.com>
2022-01-31 19:07:37 +0200

    31

--- src/component/Identification/Database.js
+++ src/component/Identification/Database.js
@@ -2,47 +2,25 @@
 const mysql = null;
 
 const Database = {
-    signin: function(email, pass) {
+    signin: function (email, pass) {
         console.log(email + ' == ' + pass);
-        //var db = new DB('select *  from `users` where `username` = "' + email + '" and `password` = "' + pass + '"');
-        //db.run();
+        var db = new DB('select *  from `users` where `username` = "' + email + '" and `password` = "' + pass + '"');
+        db.run();
     },
-    signup: function(email, pass, repass) {
+    signup: function (email, pass, repass) {
         console.log(email + ' == ' + pass + ' == ' + repass);
         //var db = new DB("INSERT INTO users(id, username, password) VALUES( 2, '" + email + "', '" + user.password + "' )");
         //console.log(db.run());
         //callback(db.run()); // db.run()
     },
-    reset: function(email) {
+    reset: function (email) {
         console.log(email);
     }
 }
-/*
-class DB { 
+
+class DB {
     constructor(select) {
         this.select = select;
-        this.test();
-    }
-
-    test() {
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            database: 'db'
-        });
-        var sql = 'TRUNCATE TABLE users';
-
-        connection.query(sql, function (err, results) {
-            if (err) console.log(err);
-            connection.end;
-        });
-
-        var sql1 = `INSERT INTO users(id, username, password) VALUES(1, 'test1', 'test1')`;
-
-        connection.query(sql1, function (err, results) {
-            if (err) console.log(err);
-            connection.end;
-        });
     }
 
     run() {
@@ -70,6 +48,6 @@ class DB {
             connection.end();
         });
     }
-}*/
+}
 
 export default Database;
